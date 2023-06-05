99eaf27f0b87797b760650b2484eaa9a11e19d10
juliogsmacedo <julio.gsmacedocoding@gmail.com>
2023-06-05 15:27:27 +0100

    cenas

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -11,7 +11,7 @@ export default function GameCard({
 
 
     return (
-        <div className="text-white flex items-center w-screen" >
+        <div className=" w-[347px] h-[99px] text-white flex items-center w-screen rounded-lg border-primaryBlue border-solid border border-opacity-10 justify-items-end" >
             <div className="flex flex-col w-1/3">
                 Imagem
                 <Image src={img} width={0} height={0} />
@@ -19,17 +19,19 @@ export default function GameCard({
 
             <div className="flex flex-col w-1/3">
                 
-                <p>{field}Ringue da Matriz</p>
+                <p className="text-contrastOffWhite font-semibold text-m">{field}Ringue da Matriz</p>
 
                 <p>{location}Póvoa de Varzim</p>
-                <span>{schedule}16:00</span>
-                <span>{date}13/2/2023</span>
-            </div>
-            <div>
-                <div className="flex w-1/3">
-                    <span>{numPlayer}6/10</span>
+                <div className="space-x-1 ">
+                <span className="text-xs text-contrastOffWhite">{schedule}16:00</span>
+                <span className="text-xs text-contrastOffWhite">{date}13/2/2023</span>
                 </div>
             </div>
+
+                <div className=" w-[71px]h-[21px] px-3 bg-primaryBlue rounded-md space-around">
+                    <span>{numPlayer}6/10</span>
+                </div>
+
         </div>
     );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,17 @@
 import Image from "next/image";
 import Button from "@/components/Button";
 import Card from "@/components/Card";
-<<<<<<< HEAD
-import CardJogo from "@/components/CardJogo";
-import CardGpt from "@/components/CardGpt";
-=======
 import { useState } from "react";
 import LoginForm from "@/components/LoginForm";
->>>>>>> 03c095812353b1a6eb5780dc9d1edbb9b86a65f1
 
 export default function Home() {
 
 
   return (
     <>
-<<<<<<< HEAD
-      <Button variant="transparent" text="Sign Up" />
-      <Button color="blue" text="Log In" />
-      {/* <Card/> */}
-      <CardJogo />
-      <CardGpt name={"Parque Tenis"} location={"Vila do Conde"} time={"9:00-22:00"} type={"Sintetico"} price={"    25     €"} />
-=======
     <h1 className="text-contrastOffWhite">HOME</h1>
     
 
->>>>>>> 03c095812353b1a6eb5780dc9d1edbb9b86a65f1
 
     </>
   );
