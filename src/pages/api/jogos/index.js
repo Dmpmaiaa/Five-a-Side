cdedbe2c40cea8ba200979eeab032e87c51f02ad
SDias7 <solangedias7@hotmail.com>
2023-06-09 09:52:14 +0100

    cenas a serem cenas

--- src/pages/api/jogos/index.js
+++ src/pages/api/jogos/index.js
@@ -1,45 +1,48 @@
-import {checkDataFromNewGame} from "@/server/middleware/jogos";
+import { checkDataFromNewGame } from "@/server/middleware/jogos";
 
 import {
-    getGames,
-    getGamesByDate,
-    getGamesByLocation,
-    newGame,
+  getGames,
+  getGamesByDate,
+  getGamesByLocation,
+  newGame,
 } from "@/server/services/games";
 
 export default async function handler(req, res) {
-    try {
-        const location = req.query.location;
-        const date = req.query.date;
+  try {
+    const location = req.query.location;
+    const date = req.query.date;
 
 
-        if (req.method === "GET") {
-            if (location) {
-                const games = await getGamesByLocation(location);
-                if (games) {
-                    return res.status(200).json(games);
-                }
-                return res.status(404).json(/* MENSAGEM DE ERRO */);
-            }
+    if (req.method === "GET") {
+      if (location) {
+        const games = await getGamesByLocation(location);
+        if (games) {
+          return res.status(200).json(games);
+        }
+        return res.status(404).json(/* MENSAGEM DE ERRO */);
+      }
 
-            if (date) {
-                const games = await getGamesByDate(date);
-                if (games) {
-                    return res.status(200).json(games);
-                }
-                return res.status(404).json(/* MENSAGEM DE ERRO */);
-            }
+      if (date) {
+        const games = await getGamesByDate(date);
+        if (games) {
+          return res.status(200).json(games);
+        }
+        return res.status(404).json(/* MENSAGEM DE ERRO */);
+      }
 
-    if (req.method === "POST") {
-      checkDataFromNewGame(req, res)
-      // verificaCoisas(req,res)
-      // verificaData (req,res)
-      const data = req.body;
-      console.log(data);
-      const dataForGame = await newGame(data);
-      return res.status(201).json({ dataForGame });
+      if (req.method === "POST") {
+        checkDataFromNewGame(req, res)
+        // verificaCoisas(req,res)
+        // verificaData (req,res)
+        console.log('OCARALHO')
+        const data = req.body;
+        console.log(data);
+        const dataForGame = await newGame(data);
+        return res.status(201).json({ dataForGame });
+      }
     }
-  } catch (err) {
+  }
+  catch (err) {
     console.log(err);
   }
 }
--- src/server/middleware/jogos.js
+++ src/server/middleware/jogos.js
@@ -4,13 +4,11 @@ export function checkDataFromNewGame(req, res) {
     const { idCampo, idHost, idPlayers, playerNumbers, date, schedule } = req.body;
     console.log("olaaaa")
     if (!idCampo || !idHost  || !playerNumbers || !date || !schedule) {
-        return res.status(403).end()//.json({ error: "Missing required endpoints" });
-    } if ((idPlayers.length + playerNumbers) > 10) {
-        return res.status(400).json({ error: "Number of idPlayers does not match playerNumbers" });
-        
-    } else {
-        
-        return res.status(200).end()
+         res.status(403).end()//.json({ error: "Missing required endpoints" });
+    }if((idPlayers.length + playerNumbers) > 10) {
+         res.status(400).json({ error: "Number of idPlayers does not match playerNumbers" });
+    }else{
+         res.status(200).end()
     }
 }
 
